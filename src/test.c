59a23ab45760290c62bd37aa035480f880cf2276
Amanda Sin <81495532+Andyuks@users.noreply.github.com>
2023-01-14 12:02:19 +0100

    creo q me he patinado (comienzo threadizacion)

--- src/test.c
+++ src/test.c
@@ -17,6 +17,22 @@ void forward_pass_test(nn_t *nn, double *input, double **A){
     }
 }
 
+
+void cuda_forward_pass_test(nn_t *nn, double *input, double **A)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if(i < nn->layers_size[0])
+        A[0][i] = input[i];
+    
+    
+    if(i > 0 && i < nn->n_layers)
+	{	
+        cuda_matrix_mul_add(A[i], nn->WH[i - 1], A[i - 1],  nn->layers_size[i], nn->layers_size[i - 1], nn->layers_size[i - 1], 1, nn->BH[i - 1]);  
+        cuda_matrix_func(A[i], A[i], nn->layers_size[i], 1, nn->activation_ptr[i - 1]);
+    }
+}
+
 //printf("Expected: %f , Obtained: %f Loss %f\n", output[0], A[nn->n_layers - 1][0], loss);
 
 
--- src/train.c
+++ src/train.c
@@ -82,3 +82,91 @@ void update(nn_t *nn, double **D, double **d, double lr, int batch_size){
 }
 
 #endif
+
+
+
+
+#ifdef GPU
+
+#include "train.h"
+
+void forward_pass(nn_t *nn, double *input, double **A, double **Z){
+
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if(idx < nn->layers_size[0])
+        A[0][i] = input[i];
+    
+    if(i > 0 && i < nn->n_layers)
+	{	
+        cuda_matrix_mul_add(Z[i], nn->WH[i - 1], A[i - 1],  nn->layers_size[i], nn->layers_size[i - 1], nn->layers_size[i - 1], 1, nn->BH[i - 1]);  
+        cuda_matrix_func(A[i], Z[i], nn->layers_size[i], 1, nn->activation_ptr[i - 1]);
+        cuda_matrix_func(Z[i], Z[i], nn->layers_size[i], 1, nn->dactivation_ptr[i - 1]);
+    }
+}
+
+double back_prop(nn_t *nn, double *output, double **A, double **Z, double **D, double **d)
+{
+    int i, n_l;
+    int *l_s;
+    double loss;
+    double *T;
+    double **E, **D_aux;
+
+    n_l = nn->n_layers;
+    l_s = nn->layers_size;
+
+    D_aux = cuda_alloc_matrix_2v(n_l - 1, &(l_s[1]), &(l_s[0]), init_zero);
+    E = cuda_alloc_matrix_1v(n_l - 1, &(l_s[1]), init_zero);
+
+    loss = nn->loss(A[n_l - 1], output, l_s[n_l - 1]);
+
+    cuda_matrix_sub(E[n_l - 2], A[n_l - 1], output, l_s[n_l - 1], 1);
+    cuda_matrix_mul_dot(E[n_l - 2], E[n_l - 2], Z[n_l - 1], l_s[n_l - 1], 1);  
+    
+
+    T = cuda_matrix_transpose(A[n_l - 2], l_s[n_l - 2], 1); 
+    cuda_matrix_mul(D_aux[n_l - 2], E[n_l - 2], T, l_s[n_l - 1], 1, 1, l_s[n_l - 2]);
+    cuda_matrix_free(T);
+
+    cuda_matrix_sum(D[n_l - 2], D[n_l - 2], D_aux[n_l - 2], l_s[n_l - 1], l_s[n_l - 2]);
+    cuda_matrix_sum(d[n_l - 2], d[n_l - 2], E[n_l - 2], l_s[n_l - 1], 1);
+
+    for (i = n_l - 2; i > 0; i--) {
+            
+        T = cuda_matrix_transpose(nn->WH[i], l_s[i + 1], l_s[i]);
+        cuda_matrix_mul(E[i - 1], T, E[i], l_s[i], l_s[i + 1], l_s[i + 1], 1);
+        cuda_matrix_free(T);
+
+        cuda_matrix_mul_dot(E[i - 1], E[i - 1], Z[i], l_s[i], 1);
+
+        cuda_matrix_mul(D_aux[i - 1], E[i - 1], A[i - 1], l_s[i], 1, 1, l_s[i - 1]);
+
+        cuda_matrix_sum(D[i - 1], D[i - 1], D_aux[i - 1], l_s[i], l_s[i - 1]);
+        cuda_matrix_sum(d[i - 1], d[i - 1], E[i - 1], l_s[i], 1);
+    }
+
+    cuda_matrix_free_2D(D_aux, n_l - 1);
+    cuda_matrix_free_2D(E, n_l - 1);
+
+    return(loss);
+
+}
+
+void update(nn_t *nn, double **D, double **d, double lr, int batch_size){
+
+    int i;
+
+	if(idx > 0 && idx < nn->n_layers)
+	{	
+    for(i = 0; i < nn->n_layers - 1; i++){
+        cuda_matrix_mul_cnt(D[i], nn->layers_size[i + 1], nn->layers_size[i],  lr * (1.0 / batch_size));
+        cuda_matrix_mul_cnt(d[i], nn->layers_size[i + 1], 1,  lr * (1.0 / batch_size));
+        cuda_matrix_sub(nn->WH[i], nn->WH[i], D[i],  nn->layers_size[i + 1], nn->layers_size[i]);
+        cuda_matrix_sub(nn->BH[i], nn->BH[i], d[i],  nn->layers_size[i + 1], 1);
+        cuda_matrix_zero(D[i], nn->layers_size[i + 1], nn->layers_size[i]);
+        cuda_matrix_zero(d[i], nn->layers_size[i + 1], 1);
+    }
+}
+
+#endif
